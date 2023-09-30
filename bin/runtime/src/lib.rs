929ed0c234870567dfd2c96f5d1359b61e7049f2
Jędrzej Kula <jedrzej.kula@cardinals.cc>
2023-09-30 20:36:30 +0200

    Master Thesis update commit

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -134,11 +134,11 @@ pub const NANO_AZERO: Balance = MICRO_AZERO / 1000;
 pub const PICO_AZERO: Balance = NANO_AZERO / 1000;
 
 // 75% block weight is dedicated to normal extrinsics
-pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
+pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(100);
 // The whole process for a single block should take 1s, of which 400ms is for creation,
 // 200ms for propagation and 400ms for validation. Hence the block weight should be within 400ms.
 pub const MAX_BLOCK_WEIGHT: Weight =
-    Weight::from_ref_time(WEIGHT_REF_TIME_PER_MILLIS.saturating_mul(400));
+    Weight::from_ref_time(WEIGHT_REF_TIME_PER_MILLIS.saturating_mul(2000));
 
 // The storage deposit is roughly 1 TOKEN per 1kB -- this is the legacy value, used for pallet Identity and Multisig.
 pub const LEGACY_DEPOSIT_PER_BYTE: Balance = MILLI_AZERO;
--- relations/src/shielder/vote.rs
+++ relations/src/shielder/vote.rs
@@ -36,7 +36,7 @@ mod relation {
             fields::fp::FpVar,
         },
         ark_relations::ns,
-        ark_serialize::CanonicalSerialize,
+        ark_serialize::{CanonicalDeserialize, CanonicalSerialize},
         ark_std::ops::Add,
         sha2::Sha256,
     };
@@ -58,8 +58,8 @@ mod relation {
     struct VoteRelation {
         #[constant]
         pub max_path_len: u8,
-        #[constant(frontend_type = "FrontendVoteBases", parse_with = "convert_bases")]
-        pub vote_bases: BackendVoteBases,
+        //#[constant(frontend_type = "FrontendVoteBases", parse_with = "convert_bases")]
+        //pub vote_bases: BackendVoteBases,
 
         // Public inputs
         #[public_input(frontend_type = "FrontendTokenId")]
@@ -116,6 +116,41 @@ mod relation {
         let second_vote_hash =
             FpVar::new_input(ns!(cs, "second vote hash"), || self.second_vote_hash())?;
 
+        let bases = [
+            ark_bls12_381::G1Affine::deserialize_compressed(
+                &[
+                    131u8, 243, 22, 251, 27, 15, 154, 154, 252, 137, 52, 42, 231, 183, 121, 207,
+                    68, 95, 68, 69, 244, 238, 227, 27, 58, 108, 44, 150, 223, 140, 129, 232, 31,
+                    152, 214, 153, 240, 95, 130, 13, 132, 10, 101, 131, 236, 124, 12, 44,
+                ][..],
+            )
+            .unwrap(),
+            ark_bls12_381::G1Affine::deserialize_compressed(
+                &[
+                    153, 79, 94, 143, 147, 208, 228, 13, 192, 64, 24, 57, 66, 193, 85, 11, 195, 75,
+                    28, 217, 165, 46, 233, 4, 104, 89, 98, 228, 229, 161, 118, 59, 199, 47, 89, 93,
+                    60, 84, 126, 107, 97, 183, 40, 255, 177, 20, 52, 140,
+                ][..],
+            )
+            .unwrap(),
+            ark_bls12_381::G1Affine::deserialize_compressed(
+                &[
+                    170, 102, 22, 74, 123, 164, 5, 124, 121, 139, 107, 175, 157, 91, 212, 41, 60,
+                    183, 33, 138, 222, 56, 117, 34, 45, 87, 244, 111, 197, 10, 199, 246, 122, 78,
+                    75, 81, 145, 211, 131, 106, 162, 251, 14, 168, 47, 119, 102, 169,
+                ][..],
+            )
+            .unwrap(),
+            ark_bls12_381::G1Affine::deserialize_compressed(
+                &[
+                    178, 43, 151, 216, 24, 165, 67, 133, 253, 118, 49, 69, 225, 146, 160, 252, 192,
+                    121, 8, 170, 211, 191, 186, 248, 70, 255, 103, 115, 159, 176, 219, 76, 86, 143,
+                    188, 54, 131, 49, 236, 214, 72, 12, 34, 69, 66, 151, 223, 11,
+                ][..],
+            )
+            .unwrap(),
+        ];
+
         let first_vote = FpVar::new_witness(ns!(cs, "first vote"), || match self.first_vote() {
             Ok(v) => Ok(CircuitField::from(*v)),
             Err(e) => Err(e),
@@ -131,7 +166,7 @@ mod relation {
         let vote_randomness =
             FpVar::new_witness(ns!(cs, "randomness vote"), || self.vote_randomness())?;
 
-        let bases = self.vote_bases();
+        //let bases = self.vote_bases();
 
         let first_hash = bases[0]
             .mul_bigint(vote_randomness.value().unwrap_or_default().into_bigint())
@@ -270,7 +305,6 @@ mod tests {
 
         VoteRelationWithFullInput::new(
             MAX_PATH_LEN,
-            VOTE_BASES.to_vec(),
             token_id,
             nullifier,
             token_amount,
@@ -304,8 +338,7 @@ mod tests {
 
     #[test]
     fn vote_proving_procedure() {
-        let circuit_without_input =
-            VoteRelationWithoutInput::new(MAX_PATH_LEN, VOTE_BASES.to_vec());
+        let circuit_without_input = VoteRelationWithoutInput::new(MAX_PATH_LEN);
 
         let mut rng = ark_std::rand::rngs::StdRng::from_rng(ark_std::test_rng()).unwrap();
 
@@ -324,8 +357,7 @@ mod tests {
 
     #[test]
     fn cannot_vote_partially_sub() {
-        let circuit_without_input =
-            VoteRelationWithoutInput::new(MAX_PATH_LEN, VOTE_BASES.to_vec());
+        let circuit_without_input = VoteRelationWithoutInput::new(MAX_PATH_LEN);
 
         let mut rng = ark_std::rand::rngs::StdRng::from_rng(ark_std::test_rng()).unwrap();
 
@@ -346,8 +378,7 @@ mod tests {
 
     #[test]
     fn cannot_vote_partially_add() {
-        let circuit_without_input =
-            VoteRelationWithoutInput::new(MAX_PATH_LEN, VOTE_BASES.to_vec());
+        let circuit_without_input = VoteRelationWithoutInput::new(MAX_PATH_LEN);
 
         let mut rng = ark_std::rand::rngs::StdRng::from_rng(ark_std::test_rng()).unwrap();
 
@@ -368,8 +399,7 @@ mod tests {
 
     #[test]
     fn cannot_vote_with_wrong_vote_hash() {
-        let circuit_without_input =
-            VoteRelationWithoutInput::new(MAX_PATH_LEN, VOTE_BASES.to_vec());
+        let circuit_without_input = VoteRelationWithoutInput::new(MAX_PATH_LEN);
 
         let mut rng = ark_std::rand::rngs::StdRng::from_rng(ark_std::test_rng()).unwrap();
 
@@ -390,8 +420,7 @@ mod tests {
 
     #[test]
     fn cannot_vote_with_wrong_vote_randomness() {
-        let circuit_without_input =
-            VoteRelationWithoutInput::new(MAX_PATH_LEN, VOTE_BASES.to_vec());
+        let circuit_without_input = VoteRelationWithoutInput::new(MAX_PATH_LEN);
 
         let mut rng = ark_std::rand::rngs::StdRng::from_rng(ark_std::test_rng()).unwrap();
 
